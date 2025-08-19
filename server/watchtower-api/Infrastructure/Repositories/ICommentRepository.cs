a21aee8912b38b90f3a054a620159c1aa6e640b1
hkayy33 <hkayy33@gmail.com>
2025-08-19 10:42:03 +0100

    Implemented Infrastructure interface code

---
+++ server/watchtower-api/Infrastructure/Repositories/ICommentRepository.cs
@@ -0,0 +1,15 @@
+using WatchtowerApi.Domain;
+
+namespace WatchtowerApi.Infrastructure.Repositories;
+
+public interface ICommentRepository
+{
+    Task<Comment> CreateAsync(int reportId, int userId, string commentText);
+    Task<Comment?> GetByIdAsync(int id);
+    Task<IEnumerable<Comment>> GetByReportIdAsync(int reportId);
+    Task<IEnumerable<Comment>> GetByUserIdAsync(int userId);
+    Task<Comment> UpdateAsync(int id, string commentText);
+    Task DeleteAsync(int id);
+    Task<int> GetCountByReportAsync(int reportId);
+    Task<int> GetCountByUserAsync(int userId);
+}
---
+++ server/watchtower-api/Infrastructure/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+using WatchtowerApi.Domain;
+
+namespace WatchtowerApi.Infrastructure.Repositories;
+
+public interface IUserRepository
+{
+    Task<User> CreateAsync(string email, string password, string username);
+    Task<User?> GetByIdAsync(int id);
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByUsernameAsync(string username);
+    Task<User?> VerifyCredentialsAsync(string email, string password);
+    Task<User> UpdateAsync(User user);
+    Task DeleteAsync(int id);
+    Task<bool> EmailExistsAsync(string email);
+    Task<bool> UsernameExistsAsync(string username);
+}
