8a4de1b210fdb0a1fea1e239f7a8a0861ca56b47
hkayy33 <hkayy33@gmail.com>
2025-08-20 17:05:54 +0100

    Comments controller and Reports Controller partially implemented

--- server/watchtower-api/Infrastructure/Repositories/ICommentRepository.cs
+++ server/watchtower-api/Infrastructure/Repositories/ICommentRepository.cs
@@ -5,9 +5,9 @@ namespace WatchtowerApi.Infrastructure.Repositories;
 public interface ICommentRepository
 {
     Task<Comment> CreateAsync(int reportId, int userId, string commentText);
-    Task<Comment?> GetByIdAsync(int id);
-    Task<IEnumerable<Comment>> GetByReportIdAsync(long reportId);
-    Task<IEnumerable<Comment>> GetByUserIdAsync(long userId);
+    Task<Comment?> GetByIdAsync(long id);
+    Task<IEnumerable<Comment>> GetByReportIdAsync(int reportId);
+    Task<IEnumerable<Comment>> GetByUserIdAsync(int userId);
     Task<Comment> UpdateAsync(int id, string commentText);
     Task DeleteAsync(int id);
     Task<int> GetCountByReportAsync(int reportId);
